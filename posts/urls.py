6422258ef139630237dc534095cbc61f9fe4099e
NwekeMaxwell <nwekemaxwell37@gmail.com>
2024-09-19 12:58:35 +0100

    feat: dynamic url routing

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home),
+    path("home/", views.home, name='home'),
     # path("<id>/", views.post), #this path allows the id to be any type of data type
     path("<int:id>/", views.post, name='post') #this path allows the id to be sprcified type of data type, the name parameter is used for redirect
 ]
