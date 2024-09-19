0c703e1c98d42cd03f01529f8fdceeae0831ea78
NwekeMaxwell <nwekemaxwell37@gmail.com>
2024-09-19 15:57:19 +0100

    feat: db management

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Feature(models.Model): #correct way to define a working model 
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+
+# class Feature:  ----------you can import this to your view and use it as your mock model for your components
+#     id: int
+#     name: str
+#     details: str
